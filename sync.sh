6fc37be185f450f262a321aa3c27921334ff08ec
totoleheros <thomasbrenard@gmail.com>
2022-11-01 13:47:11 +0100

    simplify update to page

--- sync.sh
+++ sync.sh
@@ -9,9 +9,9 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-git checkout gh-pages
-git merge main # bring gh-pages up to date with master
-git push origin gh-pages #commit the changes
-git checkout main #return to the main branch
+git checkout main
+git merge dev # bring gh-pages up to date with master
+git push origin main #commit the changes
+git checkout dev
 
 cd -
