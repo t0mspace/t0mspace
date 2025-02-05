3ac82cdbea7a455e36e94563a69b013ad6bfd06c
Thomas Brenard <thomasx2005@gmail.com>
2025-02-05 15:53:28 +0100

    temp delete nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,9 @@ import './header.scss';
 function Header() {
     return (
         <div>
-            <header className="header w-full p-2 lg:flex items-center justify-around">
+            <header className="header w-full p-2 lg:flex text-left">
                 <h1 className="header__title font-bold text-3xl text-white">Tom<span>'</span>space</h1>
-              <nav className="header__menu hidden lg:flex flex-row flex-nowrap text-3xl">
+              <nav className="header__menu hidden">
                 <li className="header__menu-item"><a href="">Home</a></li>
                 <li className="header__menu-item"><a>Skills</a></li>
                 <li className="header__menu-item"><a>Projects</a></li>
