5311a37a16382be09b2ce75be4a4f7a550388120
Thomas Brenard <thomasx2005@gmail.com>
2025-02-05 11:42:28 +0100

    beautify hypertext links

--- src/components/About/AboutSection.tsx
+++ src/components/About/AboutSection.tsx
@@ -4,7 +4,7 @@ function AboutSection() {
       <h2 className="text-4xl mb-16 font-bold text-center">About</h2>
       <div className="flex max-w-3xl">
         <p>I made this website firstly to find a new job :D, and express my creativity.<br />
-        In a technical point of view, this website works with ReactJs and TailwindCSS and is hosted in a github-page.
+        In a technical point of view, this website works with <a className="external-link" href="//react.dev">ReactJs</a> and <a className="external-link" href="//v3.tailwindcss.com">TailwindCSS</a> and is hosted in a github-page.
           </p>
       </div>
     </div>
--- src/components/Contact/ContactSection.tsx
+++ src/components/Contact/ContactSection.tsx
@@ -1,5 +1,6 @@
 import linkedin from '../../assets/images/icons8-linkedin.svg'
 import github from '../../assets/images/icons8-github.svg'
+import { link } from 'framer-motion/client'
 
 function ContactSection() {
   const data = {
@@ -28,24 +29,24 @@ function ContactSection() {
       <h2 className="text-4xl mb-16 font-bold text-center">Contact</h2>
       <div className="text-left">
         <p className="text-gray-700 dark:text-gray-200 mb-6 transition-colors duration-200">Feel free to reach out to me
-          via
-          email or connect on social media!
+          via &nbsp;
+          <span className="external-link" onClick={openEmailClient}>email</span>
         </p>
-        <button onClick={openEmailClient}
-                className="bg-primary text-white hover:bg-primary-400 font-semibold py-2 px-4 rounded-md transition-all duration-300 text-sm md:text-base flex items-center justify-center mx-auto mb-6 shadow-lg hover:shadow-xl ease-in-out transform hover:-translate-y-1 bg-gradient-to-bl from-primary-400 to-primary-600 hover:from-primary-500 hover:to-primary-700 dark:from-primary-500 dark:to-primary-700 dark:hover:from-primary-600 dark:hover:to-primary-800 focus:ring-2 focus:ring-primary-300 dark:focus:ring-primary-600 focus:outline-none">
-          Send me an email
-        </button>
+
         <div className="flex  space-x-4">
+          <p> or connect on social media!</p>
           {data.socialLinks.map((item, index) => (
             <a key={index} href={item.url} target="_blank"
                rel="noopener noreferrer"
-               className="text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-400 transition-colors
+               className="cursor-pointer text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-400 transition-colors
         duration-300 focus:outline-none focus:ring-2 focus:ring-primary-300 dark:focus:ring-primary-600 rounded-full">
               <img src={item.icon} alt={item.name} className="h-10 w-10" />
             </a>
           ))
           }
+
         </div>
+
       </div>
     </div>
   )
