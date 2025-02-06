9fc4035ec1e64e7251badc3800d07a4ded311515
Thomas Brenard <thomasx2005@gmail.com>
2025-02-06 12:01:13 +0100

    add scroll snap

--- src/VerticalSlider.tsx
+++ src/VerticalSlider.tsx
@@ -1,28 +1,18 @@
-import {useState} from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import "./index.scss";
+import './index.scss'
 
+const VerticalSlider = ({ sections }) => {
+  return (
+    <div className="bg-gray-900 text-white flex flex-col h-screen overflow-y-scroll scroll-snap">
+      {sections.map((Section, index) => (
+        <section
+          key={index}
+          className={`scroll-snap-child mb-16 ml-5 lg:ml-20 min-h-screen flex-${index}`}
+        >
+          <Section />
+        </section>
+      ))}
+    </div>
+  )
+}
 
-const VerticalSlider = ({sections}) => {
-    const [activeIndex] = useState(0);
-
-    return (
-        <div className=" bg-gray-900 text-white flex flex-col">
-            <AnimatePresence mode="wait">
-                <motion.div
-                    animate={{translateY: -activeIndex * 100 + "vh"}}
-                    transition={{type: "spring", stiffness: 100}}
-                    className=""
-                >
-                    {sections.map((Section, index) => (
-                        <section key={index} className={`mb-16 ml-5 lg:ml-20 min-h-screen flex-${index}`}>
-                            <Section />
-                        </section>
-                    ))}
-                </motion.div>
-            </AnimatePresence>
-        </div>
-    );
-};
-
-export default VerticalSlider;
+export default VerticalSlider
--- src/components/Home/HomeSection.tsx
+++ src/components/Home/HomeSection.tsx
@@ -12,7 +12,7 @@ const data = {
 
 function HomeSection() {
     return (
-      <div className={`${styles["home"]} flex flex-col md:h-screen items-center justify-center mb-72`}>
+      <div className={`${styles["home"]} relative flex flex-col md:h-screen items-center justify-center mb-72`}>
         <div className="home__presentation">
           <h2 className={styles["home__title"]}>👋  {data.greeting}</h2>
 
