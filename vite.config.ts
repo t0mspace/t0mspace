c435db516b158cc58822f02a4af0e812cdecaa86
Thomas Brenard <thomasx2005@gmail.com>
2025-03-25 18:28:37 +0100

    improve deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/t0mspace/",
+  base:"/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
