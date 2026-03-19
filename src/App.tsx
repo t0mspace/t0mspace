8d9b687970b20721ab40357710bb4112e85c3315
Thomas B. <thomasx2005@gmail.com>
2026-03-19 19:20:10 +0100

    fix bug with vertical line on the right

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,16 @@ import StarField from "./components/Background/background.tsx";
 import ProjectsSection from "./components/Projects/ProjectsSection.tsx";
 import ContactSection from "./components/Contact/ContactSection.tsx";
 function App() {
-    const sections = [HomeSection, SkillsSection, ProjectsSection,AboutSection, ContactSection];
-    return (
-        <div className="bg-cover h-screen bg-gray-900 text-white -z-3">
-            <div className="absolute -z-3 inset-0">
-                <StarField starCount={150}/>
-                <Header/>
-                <VerticalSlider sections={sections}/>
-            </div>
-        </div>
-    );
+  const sections = [HomeSection, SkillsSection, ProjectsSection, AboutSection, ContactSection];
+  return (
+    <div className="bg-cover h-screen bg-gray-900 text-white">
+      <div className="absoluteinset-0">
+        <StarField starCount={150} />
+        <Header />
+        <VerticalSlider sections={sections} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/VerticalSlider.tsx
+++ src/VerticalSlider.tsx
@@ -2,11 +2,11 @@ import './index.scss'
 
 const VerticalSlider = ({ sections }) => {
   return (
-    <div className="bg-gray-900 text-white flex flex-col min-h-screen overflow-y-scroll scroll-snap">
+    <div className="bg-gray-900 text-white flex flex-col min-h-screen">
       {sections.map((Section, index) => (
         <section
           key={index}
-          className={`scroll-snap-child mb-16 ml-5 lg:ml-20 min-h-screen flex-${index}`}
+          className={`scroll-snap-child mb-16 min-h-screen flex-${index}`}
         >
           <Section />
         </section>
