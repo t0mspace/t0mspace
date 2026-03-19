870c4d615a2e3fca4f88ea6fbbb3cc43522e020b
Thomas B. <thomasx2005@gmail.com>
2026-03-19 18:37:28 +0100

    fix react error

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,112 +1,112 @@
 import './header.scss';
 import { useState } from 'react'
-import {data} from '../Common/infos'
+import { data } from '../Common/infos'
 
 
 function Header() {
-    const [isNavOpen, setIsNavOpen] = useState(false);
-    return (
-        <div>
-          <header className="flex items-center justify-around sm:p-2 lg:p-8">
-            <h1 className="header__title font-bold text-3xl text-white">T<span className="o">&#216;</span>m<span className="apostrophe">&#x2019;</span>s space</h1>
-            <nav>
-              <section className="MOBILE-MENU flex lg:hidden">
-                <div
-                  className="HAMBURGER-ICON space-y-2"
-                  onClick={() => setIsNavOpen((prev) => !prev)}
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  return (
+    <div>
+      <header className="flex items-center justify-around sm:p-2 lg:p-8">
+        <h1 className="header__title font-bold text-3xl text-white">T<span className="o">&#216;</span>m<span className="apostrophe">&#x2019;</span>s space</h1>
+        <nav>
+          <section className="MOBILE-MENU flex lg:hidden">
+            <div
+              className="HAMBURGER-ICON space-y-2"
+              onClick={() => setIsNavOpen((prev) => !prev)}
+            >
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            </div>
+
+            <div className={isNavOpen ? 'showMenuNav' : 'hideMenuNav'}>
+              <div
+                className="absolute top-0 right-0 px-8 py-8"
+                onClick={() => setIsNavOpen(false)}
+              >
+                <svg
+                  className="h-8 w-8 text-gray-600"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
-                  <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-                  <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-                  <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-                </div>
-
-                <div className={isNavOpen ? 'showMenuNav' : 'hideMenuNav'}>
-                  <div
-                    className="absolute top-0 right-0 px-8 py-8"
-                    onClick={() => setIsNavOpen(false)}
-                  >
-                    <svg
-                      className="h-8 w-8 text-gray-600"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <line x1="18" y1="6" x2="6" y2="18" />
-                      <line x1="6" y1="6" x2="18" y2="18" />
-                    </svg>
-                  </div>
-                  <ul className="flex flex-col items-start justify-between min-h-[250px]">
-                    <li className=" my-8 uppercase">
-                      <a href="#home">Home</a>
-                    </li>
-                    <li className=" my-8 uppercase">
-                      <a href="#skills">Skills</a>
-                    </li>
-                    <li className=" my-8 uppercase">
-                      <a href="#projects">Projects</a>
-                    </li>
-                    <li className="my-8 uppercase">
-                      <a href="//medium.com/@thomasx2005">Blog</a>
-                    </li>
-                    <li className="my-8 uppercase">
-                      <a href="#about">About</a>
-                    </li>
-                    <li className=" my-8 uppercase">
-                      <a href="#contact">Contact</a>
-                    </li>
-                  </ul>
-                  <ul className="list-unstyled flex flex-row flex-nowrap mt-8 gap-4">
-                  {data.socialLinks.map((item, index) => (
-                    <li><a key={index} href={item.url} target="_blank"
-                       rel="noopener noreferrer"
-                       className="cursor-pointer text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-400 transition-colors
-        duration-300 focus:outline-none focus:ring-2 focus:ring-primary-300 dark:focus:ring-primary-600 rounded-full">
-                      <img src={item.icon} alt={item.name} className="h-10 w-10" />
-                    </a></li>
-                  ))
-                  }
-                  </ul>
-                </div>
-
-              </section>
-
-              <ul className="hidden space-x-8 lg:flex">
-                <li>
+                  <line x1="18" y1="6" x2="6" y2="18" />
+                  <line x1="6" y1="6" x2="18" y2="18" />
+                </svg>
+              </div>
+              <ul className="flex flex-col items-start justify-between min-h-[250px]">
+                <li className=" my-8 uppercase">
                   <a href="#home">Home</a>
                 </li>
-                <li>
+                <li className=" my-8 uppercase">
                   <a href="#skills">Skills</a>
                 </li>
-                <li>
+                <li className=" my-8 uppercase">
                   <a href="#projects">Projects</a>
                 </li>
-                <li>
+                <li className="my-8 uppercase">
                   <a href="//medium.com/@thomasx2005">Blog</a>
                 </li>
-                <li>
+                <li className="my-8 uppercase">
                   <a href="#about">About</a>
                 </li>
-                <li>
+                <li className=" my-8 uppercase">
                   <a href="#contact">Contact</a>
                 </li>
-                {data.socialLinks.map((item, index) => (
-                  <a key={index} href={item.url} target="_blank"
-                     rel="noopener noreferrer"
-                     className="cursor-pointer text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-400 transition-colors
-        duration-300 focus:outline-none focus:ring-2 focus:ring-primary-300 dark:focus:ring-primary-600 rounded-full">
-                    <img src={item.icon} alt={item.name} className="h-10 w-10" />
-                  </a>
-                ))
-                }
-
               </ul>
-            </nav>
-          </header>
-        </div>
-    )
+              <ul className="list-unstyled flex flex-row flex-nowrap mt-8 gap-4">
+                {data.socialLinks.map((item) => (
+                  <li key={item.name}>
+                    <a href={item.url} target="_blank"
+                      rel="noopener noreferrer"
+                      className="cursor-pointer text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-400 transition-colors
+         duration-300 focus:outline-none focus:ring-2 focus:ring-primary-300 dark:focus:ring-primary-600 rounded-full">
+                      <img src={item.icon} alt={item.name} className="h-10 w-10" />
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+          </section>
+
+          <ul className="hidden space-x-8 lg:flex">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="//medium.com/@thomasx2005">Blog</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            {data.socialLinks.map((item) => (
+              <a key={item.name} href={item.url} target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-400 transition-colors
+     duration-300 focus:outline-none focus:ring-2 focus:ring-primary-300 dark:focus:ring-primary-600 rounded-full">
+                <img src={item.icon} alt={item.name} className="h-10 w-10" />
+              </a>
+            ))}
+
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
 }
 
 export default Header;
--- src/components/Skills/SkillsSection.tsx
+++ src/components/Skills/SkillsSection.tsx
@@ -9,48 +9,48 @@ import git from "../../assets/images/skills/icons8-git.svg";
 import sql from "../../assets/images/skills/icons8-sql.svg";
 
 function SkillsSection() {
-    const skills = [
-        { name: "HTML", src: html },
-        { name: "CSS", src: css },
-        { name: "JavaScript", src: javascript },
-        { name: "PHP", src: php },
-        { name: "Symfony", src: symfony },
-        { name: "Linux", src: linux },
-        { name: "Git", src: git },
-        { name: "SQL", src: sql },
-    ];
+  const skills = [
+    { name: "HTML", src: html },
+    { name: "CSS", src: css },
+    { name: "JavaScript", src: javascript },
+    { name: "PHP", src: php },
+    { name: "Symfony", src: symfony },
+    { name: "Linux", src: linux },
+    { name: "Git", src: git },
+    { name: "SQL", src: sql },
+  ];
 
-    return (
-        <div id={"skills"} className="justify-center skills-section max-w-3xl mx-auto">
-          <h2 className="text-4xl mb-16 font-bold text-center">Skills</h2>
-          <h3 className="text-3xl mb-8 mt-16">IT</h3>
-          <div className="flex">
-            <div className="grid grid-cols-1 md:grid-cols-5 gap-4 mx-auto">
-              {skills.map((skill, index) => (
-                <div className="bg-black/.9 backdrop-blur-xl">
-                  <div key={index} className="skill-item dark:bg-slate-700/30 bg-opacity-100 p-4 rounded-lg">
-                      <img src={skill.src} alt={skill.name} className="w-auto h-20 mx-auto rounded-full" />
-                      <p className="text-center m-1">{skill.name}</p>
-                  </div>
-                </div>
-              ))}
+  return (
+    <div id={"skills"} className="justify-center skills-section max-w-3xl mx-auto">
+      <h2 className="text-4xl mb-16 font-bold text-center">Skills</h2>
+      <h3 className="text-3xl mb-8 mt-16">IT</h3>
+      <div className="flex">
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-4 mx-auto">
+          {skills.map((skill, index) => (
+            <div key={index} className="bg-black/.9 backdrop-blur-xl">
+              <div className="skill-item dark:bg-slate-700/30 bg-opacity-100 p-4 rounded-lg">
+                <img src={skill.src} alt={skill.name} className="w-auto h-20 mx-auto rounded-full" />
+                <p className="text-center m-1">{skill.name}</p>
               </div>
-          </div>
-          <h3 className="text-3xl mb-8 mt-16">Languages</h3>
-          <div className="bg-black/.9 backdrop-blur-xl p-8 text-gray-200 custom-background bg-opacity-100 dark:bg-slate-700/30 rounded-2xl transform duration-300 cursor-pointer flex flex-col justify-between">
-          <ul className="text">
-            <li>French: native</li>
-            <li>English: B2</li>
-            <li>Dutch: A2</li>
-          </ul>
-          </div>
-          <h3 className="text-3xl mb-8 mt-16">Soft skills</h3>
-          <div className="bg-black/.9 backdrop-blur-xl p-8 text-gray-200 custom-background bg-opacity-100 dark:bg-slate-700/30 rounded-2xl transform duration-300 cursor-pointer flex flex-col justify-between">
-          <p>Versatile, Sociable, empathic, creative and critical thinking</p>
-          </div>
+            </div>
+          ))}
         </div>
+      </div>
+      <h3 className="text-3xl mb-8 mt-16">Languages</h3>
+      <div className="bg-black/.9 backdrop-blur-xl p-8 text-gray-200 custom-background bg-opacity-100 dark:bg-slate-700/30 rounded-2xl transform duration-300 cursor-pointer flex flex-col justify-between">
+        <ul className="text">
+          <li>French: native</li>
+          <li>English: B2</li>
+          <li>Dutch: A2</li>
+        </ul>
+      </div>
+      <h3 className="text-3xl mb-8 mt-16">Soft skills</h3>
+      <div className="bg-black/.9 backdrop-blur-xl p-8 text-gray-200 custom-background bg-opacity-100 dark:bg-slate-700/30 rounded-2xl transform duration-300 cursor-pointer flex flex-col justify-between">
+        <p>Versatile, Sociable, empathic, creative and critical thinking</p>
+      </div>
+    </div>
 
-    )
+  )
 }
 
 export default SkillsSection;
