96f0660ebc7b1ca05b03b0f346199eb8c5228512
Thomas B. <thomasx2005@gmail.com>
2026-03-19 15:54:44 +0100

    update about  section

--- src/components/About/AboutSection.tsx
+++ src/components/About/AboutSection.tsx
@@ -7,19 +7,12 @@ function AboutSection() {
         <p>I made this website firstly to find a new job :D, and express my creativity.<br />
         In a technical point of view, this website works with <a className="external-link" href="//react.dev">ReactJs</a> and <a className="external-link" href="//v3.tailwindcss.com">TailwindCSS</a> and is hosted in a github-page.
           </p>
-        <p>I have 5 years of experience in POO, especially in PHP (the first language I learnt) and vanilla javascript,<br />
+        <p>I have 5 years of experience in programming, especially in PHP (the first language I learnt),<br />
         and I use Docker regularly because it's an amazing tool to switch easily between different database (for ex: Mysql and PostgreSQL) and from project to another.</p>
-        <p>I also worked on a Java project using Spring Boot 3, Angular and microservices (3) when I was on the bench, 3 months ago.<br/>
-          It wasn't a piece of cake like said the project manager: it was really intensive but also really challenging and interesting:
-        I love to be autonomous on the choice of technologies and touch a bit to everything (dev in Java + Angular + Linux administration).
-        I was fortunately not alone, a colleague helped me to setup the server (when I was too desperate by an issue and ready to throw myself by the window)</p>
-        <br />
-        <p>Here's the technologies we installed in the vps: Docker (root-less), firewall-d, Spring Boot, PostgreSQL, GitLab and Traefik as reverse proxy.
-        But the other side of the coin when I have the cap of dev, devops, and sysadmin is that I become a bit too much scattered.</p>
-        <br />
-        <p>It was the occasion to become a mentor of an another colleague who came from Microsoft technologies, kew nothing on Linux and java and wanted to learn Angular.
-        I noticed It was not easy to explain some concept while going forward to my own tasks, but I love to share my knowledges. </p>
-
+        <p>After working severals years a developer, I recently came back to my first role: Versative IT support / helpdesk, because the developer market is become saturated. I have to say that I'm more comfortable with helping people (and I love that !) than be always in the front of a screen for coding.
+        I realized it's difficult for me to keep the motivation on working on long programming projects, especially if the project doesn't interest me very much.
+        Basically, I prefer to code on personal projects, and make my own tools.<br />
+        Computer sciences has always been a passion for me (since I was a teenager), and should stay like that.</p>
       </div>
       </div>
     </div>
